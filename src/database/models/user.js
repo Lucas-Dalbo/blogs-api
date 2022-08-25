e57b1857fa154da0fc6a690cc8f14fa23a8c1009
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-08-25 17:23:53 -0300

    feat: model User

---
+++ src/database/models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+    // underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.belongsTo(models.BlogPost,
+      { as: 'BlogPost', foreginKey: 'userId' });
+  };
+
+  return User;
+};
