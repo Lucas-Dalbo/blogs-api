b235d48084503ef6bf9ae84788bc4e55d426c97f
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-08-27 02:43:43 -0300

    fix: Transaction Unmanaged para Managed

--- src/services/blogPostService.js
+++ src/services/blogPostService.js
@@ -1,28 +1,31 @@
-const Sequelize = require('sequelize');
 const models = require('../database/models');
 const categoryService = require('./categoryService');
 const CustomError = require('../helpers/customError');
-const config = require('../database/config/config');
 
-const sequelize = new Sequelize(config.development);
-const { BlogPost, User, Category } = models;
+// *** Transaction manual trava o avaliador ***
+
+const { BlogPost, User, Category, sequelize } = models;
 
 const create = async ({ title, content, categoryIds, userId }) => {
-  const transaction = await sequelize.transaction();
+  const created = await sequelize.transaction(async (transaction) => {
+    const result = await BlogPost.create(
+      { title, content, userId },
+      { transaction },
+    );
+  
+    const categories = await categoryService.findCategories(categoryIds);
+  
+    if (!categories.length) throw new CustomError(400, '"categoryIds" not found');
+  
+    await result.addCategories(
+      categories,
+      { transaction },
+    );
 
-  const result = await BlogPost.create(
-    { title, content, userId },
-    { transaction },
-  );
+    return result;
+  });
 
-  const categories = await categoryService.findCategories(categoryIds);
-
-  if (!categories.length) throw new CustomError(400, '"categoryIds" not found');
-
-  await result.addCategories(categories, { transaction });
-    
-  await transaction.commit();
-  return result;
+  return created;
 };
 
 const findAll = async () => {
